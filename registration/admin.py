870751d6746a566a2ce971e5ffb676800b3c0cc6
chriscauley <chris@lablackey.com>
2015-07-25 23:02:18 -0400

    messeg up an indent on paste

--- registration/admin.py
+++ registration/admin.py
@@ -12,31 +12,31 @@ class RawMixin(object):
       type = db_field.rel.__class__.__name__
       if type == "ManyToOneRel":
         kwargs['widget'] = VerboseForeignKeyRawIdWidget(db_field.rel, site)
-    elif type == "ManyToManyRel":
+      elif type == "ManyToManyRel":
         kwargs['widget'] = VerboseManyToManyRawIdWidget(db_field.rel, site)
       return db_field.formfield(**kwargs)
     return super(RawMixin, self).formfield_for_dbfield(db_field, **kwargs)
 
 class RegistrationAdmin(RawMixin,admin.ModelAdmin):
-    actions = ['activate_users', 'resend_activation_email']
-    list_display = ('user', 'expired')
-    raw_id_fields = ['user']
-    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+  actions = ['activate_users', 'resend_activation_email']
+  list_display = ('user', 'expired')
+  raw_id_fields = ['user']
+  search_fields = ('user__username', 'user__first_name', 'user__last_name')
 
-    def activate_users(self, request, queryset):
-        for profile in queryset:
-            RegistrationProfile.objects.activate_user(profile.activation_key)
-    activate_users.short_description = _("Activate users")
+  def activate_users(self, request, queryset):
+    for profile in queryset:
+      RegistrationProfile.objects.activate_user(profile.activation_key)
+  activate_users.short_description = _("Activate users")
 
-    def resend_activation_email(self, request, queryset):
-        if Site._meta.installed:
-            site = Site.objects.get_current()
-        else:
-            site = RequestSite(request)
+  def resend_activation_email(self, request, queryset):
+    if Site._meta.installed:
+      site = Site.objects.get_current()
+    else:
+      site = RequestSite(request)
 
-        for profile in queryset:
-            profile.send_activation_email(site)
-    resend_activation_email.short_description = _("Re-send activation emails")
+    for profile in queryset:
+      profile.send_activation_email(site)
+  resend_activation_email.short_description = _("Re-send activation emails")
 
 
 admin.site.register(RegistrationProfile, RegistrationAdmin)
